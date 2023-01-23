ff9b005b9fb93c3af67d22c7a9c38db51429e254
arromero4 <arromero0408@gmail.com>
2023-01-22 19:48:04 -0600

    React practice

---
+++ project-2/faq-project/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import './App.css'
+import Question from './Question';
+
+const questions = [
+  {
+    id: 1,
+    title: "Is this a good product?",
+    info:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem."
+  },
+  {
+    id: 2,
+    title: "How much does it cost?",
+    info:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem."
+  },
+  {
+    id: 3,
+    title: "When can I get it?",
+    info:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem."
+  }
+];
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Project 2: FAQ/Accordion</h1>
+      <div className="container">
+        <h2>Frequently Asked Questions</h2>
+        <div className="questions">
+          {
+            questions.map((question) =>(
+              <Question key={question.id} question={question}/>
+            ))
+          }
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ project-2/faq-project/src/Question.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+import React from "react";
+
+const Question = ({ question }) => {
+
+  const [isOpen, setOpen] = useState(false)
+
+  return (
+    <section>
+      <div className={isOpen ? "open":"closed"}>
+        <h4>{question.title}</h4>
+        <button
+          onClick={() => setOpen(!isOpen)}
+        >
+          {isOpen ? "-" : "+"}
+        </button>
+      </div>
+    {isOpen && <p>{question.info}</p>}
+    </section>
+  )
+};
+
+export default Question;
